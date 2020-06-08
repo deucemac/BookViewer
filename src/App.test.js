42e0770c34c646dd5e32703d7e45c1a7e39b2a63
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-06-07 21:29:41 -0400

    date-due

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,25 @@
-// import React from 'react';
-// // import { render } from '@testing-library/react';
-// import { mount } from 'enzyme';
-// import App from './App';
+import React from 'react';
+// import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
+import App from './App';
 // import Header from './Components/Header';
-// import Input from './Components/Input';
+import Input from './Components/Input';
 
 
 
-// describe('App component', () => {
-// let component
-// beforeEach(() => {
-//   component = mount(<App />)
-// })
+describe('App component', () => {
+let component
+beforeEach(() => {
+  component = mount(<App />)
+})
 
-//   it('should have 1 header subcomponent', () => {
-//     expect(component.find(Input)).toHaveLength(1)
-//   })
-// })
+  it('should have 1 header subcomponent', () => {
+    expect(component.find(Input)).toHaveLength(1)
+  })
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+})
+
+
 
 
 
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,10 @@ export default function Footer(props) {
   return (
     <div>
       <footer>
-<h1></h1>
+        <div className='parent-footer'>
+        <h1>Discover</h1>
+          <img className='footer-icon' src='https://www.pngkit.com/png/detail/332-3320971_discovery-comments-discovery-icon-png.png' />
+          </div>
       </footer>
     </div>
   )
